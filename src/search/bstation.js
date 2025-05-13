f74dabf0c258af566cc9f4462f67ee4808241d09
RizkiOfc <tururizki21@gmail.com>
2025-05-13 17:31:09 +0700

    Create bstation.js

---
+++ src/search/bstation.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const cheerio = require('cheerio');
+
+module.exports = function (app) {
+async function bsTattion(query) {
+    const url = `https://www.bilibili.tv/id/search-result?q=${query}`;
+    const { data } = await axios({
+        method: "GET",
+        url: url,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36" //Bisa Juga "Mozilla/5.0"
+        },
+    });
+    const $ = cheerio.load(data);
+
+    let result = [];
+
+    $('.ogv__content').each((_, el) => {
+        const anime = $(el).find('i.highlights__text.highlights__text--active').text().trim();
+        const title = $(el).find('i.highlights__text').text().trim();
+        const views = $('span.meta__tips-text').text().trim();
+        const description = $('p.ogv__content-desc').text().trim();
+        const type = $(el).find('.section-title').text().trim() || "Not Found";
+        const play = 'https://www.bilibili.tv' + $(el).find('a').attr('href');
+        const image = $('.ogv__cover > a > img').attr('src');
+
+        result.push({
+            anime,
+            title,
+            views,
+            description,
+            play,
+            type,
+            image
+        });
+    });
+    return result;
+}
+  app.get('/search/bstation', async(req, res) => {
+    const { query } = req.query.q;
+    if (!query) return res.status(400).json({ status: false, message: 'Masukkan parameter q' });
+    try {
+      const result = await bsTattion(query);
+        res.json({ status: true, result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: false, message: 'Gagal mengambil data' });
+  });
+}
