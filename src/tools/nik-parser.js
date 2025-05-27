ee86b13678eb17e5edf19cfb9157b0c94d8c5f77
RizkiOfc <tururizki21@gmail.com>
2025-05-27 18:41:27 +0700

    Update nik-parser.js

--- src/tools/nik-parser.js
+++ src/tools/nik-parser.js
@@ -32,8 +32,8 @@ async function Nik(nik) {
 
 module.exports = function(app) {
 app.get('/tools/nik-parser', async (req, res) => {
-       const { apikey, query } = req.query
-       if (!global.apikeyprem.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
+       const { apikeyprem, query } = req.query
+       if (!global.apikeyprem.includes(apikeyprem)) return res.json({ status: false, error: 'Apikey invalid' })
         try {
             let anu = await Nik(query)
             res.status(200).json({
