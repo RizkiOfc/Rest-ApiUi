64d1908e1587692b1ed77783faee41e09cb78370
RizkiOfc <tururizki21@gmail.com>
2025-11-10 05:48:36 +0700

    Update brat.js

--- src/imagecreator/brat.js
+++ src/imagecreator/brat.js
@@ -3,7 +3,7 @@ app.get('/imagecreator/brat', async (req, res) => {
         try {
             const { apikey, text } = req.query
             if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-            const pedo = await getBuffer(`https://brat.caliphdev.com/api/brat?text=${text}`)
+            const pedo = await getBuffer(`https://api.siputzx.my.id/api/m/brat?text=${text}&isAnimated=false&delay=500`)
             res.writeHead(200, {
                 'Content-Type': 'image/png',
                 'Content-Length': pedo.length,
